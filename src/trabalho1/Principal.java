6c6b391160d68092984d365e2e73f083e633145f
tthiagomartinho <tthiagomartinho@gmail.com>
2015-05-09 17:55:48 -0300

    Caso não paralelo e não distribuido

--- src/trabalho1/Principal.java
+++ src/trabalho1/Principal.java
@@ -5,6 +5,9 @@
  */
 package trabalho1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -18,34 +21,23 @@ public class Principal {
      */
     public static void main(String[] args) {
 
+       List<Integer> numeros = gerarNumerosAleatorios();
+       
+        Collections.sort(numeros);
+    }
+    
+    public static List<Integer> gerarNumerosAleatorios() {
+        List<Integer> numeros = new ArrayList<>();
         Random r = new Random();
-        int n1, n2, n3; // tamanho dos vetores gerados
+        int n1; // tamanho dos vetores gerados
 
         n1 = r.nextInt(1000000);
-        n2 = r.nextInt(1000000);
-        n3 = r.nextInt(1000000);
-
         // generate a uniformly distributed int random numbers
         int[] integers = new int[n1];
         for (int i = 0; i < integers.length; i++) {
-            integers[i] = r.nextInt();
+            numeros.add(r.nextInt());
         }
 
-        // generate a uniformly distributed float random numbers
-        float[] floats = new float[n2];
-        for (int i = 0; i < floats.length; i++) {
-            floats[i] = r.nextFloat();
-        }
-
-        // generate a Gaussian normally distributed random numbers
-        double[] gaussians = new double[n3];
-        double MEAN = 100.0f;
-        double VARIANCE = 50.0f;
-        for (int i = 0; i < gaussians.length; i++) {
-            gaussians[i] = MEAN + r.nextGaussian() * VARIANCE;
-        }
-
-        System.out.println(n1 + "/" + n2 + "/" + n3);
-
+        return numeros;
     }
 }
